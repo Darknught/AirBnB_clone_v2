5300082d71fca4938bf67c33893637ca808d0216
Darknught <hassino66@yahoo.com>
2024-03-07 23:15:29 +0000

    code

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -3,40 +3,34 @@
 from fabric.api import env, put, run
 import os
 
-
-env.user = 'ubuntu'
-env.key_filename = /.ssh/school
 env.hosts = ["35.168.3.169", "52.3.243.207"]
 
 
 def do_deploy(archive_path):
     """ Function to distribute the archive """
-    if not os.path.exists(archive_path):
+    if not os.path.isfile(archive_path):
         print("Error: Archive path does not exist")
         return False
 
-    try:
-        # Upload the archive to /tmp/ directory on the web servers
-        put(archive_path, '/tmp/')
+    filename = os.path.basename(archive_path)
+    file_no_ext = os.path.splitext(filename)[0]
 
-        # extract archive to /data/web_static/releases/arc filename no ext
-        filename = os.path.basename(archive_path)
-        file_no_ext = os.path.splitext(file_name)[0]
-        run("sudo mkdir -p /data/web_static/releases/{}".format(file_no_ext))
-        run("sudo tar -xzf /tmp/{} -C /data/web_static/releases/{}/".format(
-            file_name, file_no_ext))
-
-        # Delete the uploaded archive
-        run("sudo rm /tmp/{}".format(file_name))
-
-        # Delete the symbolic link /data/web_static/current
-        run("sudo rm -rf /data/web_static/current")
-
-        # Create a new symbolic link linked to the new version of the code
-        run("sudo ln -s /data/web_static/releases/{}/ "
-            "/data/web_static/current".format(file_no_ext))
-
-        return True
-    except Exception as e:
-        print("Error:", e)
+    if put(archive_path, '/tmp/{}'.format(filename)).failed:
+        print("Error: failed to upload archive")
         return False
+
+    commands = [
+            "sudo rm -rf /data/web_static/releases/{}/".format(file_no_ext),
+            "sudo mkdir -p /data/web_static/releases/{}/".format(file_no_ext),
+            "sudo tar -xzf /tmp/{} -C /data/web_static/releases/{}/".format(filename, file_no_ext),
+            "sudo rm /tmp/{}".format(filename),
+            "sudo mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}/".format(file_no_ext, file_no_ext),
+            "sudo rm -rf /data/web_static/releases/{}/web_static".format(file_no_ext),
+            "sudo rm -rf /data/web_static/current",
+            "sudo ln -s /data/web_static/releases/{}/ /data/web_static/current".format(file_no_ext)
+            ]
+    for cmd in commands:
+        if run(cmd).failed:
+            print("Error: Command failed:", cmd)
+            return False
+    return True
